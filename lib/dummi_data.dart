dc8f85852bb4d7d5a33cd0a69378de0e0b015bef
znol1 <57703588+znol1@users.noreply.github.com>
2020-11-13 14:33:16 +0300

    started

---
+++ lib/dummi_data.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+import './models/category.dart';
+
+const DUMMY_CATEGORIES = const [
+  Category(
+    id: 'c1',
+    title: 'Italian',
+    color: Colors.purple,
+  ),
+  Category(
+    id: 'c2',
+    title: 'russian',
+    color: Colors.red,
+  ),
+  Category(
+    id: 'c3',
+    title: 'franch',
+    color: Colors.yellow,
+  ),
+  Category(
+    id: 'c4',
+    title: 'tasty',
+    color: Colors.green,
+  ),
+  Category(
+    id: 'c5',
+    title: 'stupid',
+    color: Colors.pink,
+  ),
+  Category(
+    id: 'c6',
+    title: 'disgusting',
+    color: Colors.deepPurple,
+  ),
+  Category(
+    id: 'c7',
+    title: 'nice',
+    color: Colors.black,
+  ),
+  Category(
+    id: 'c8',
+    title: 'soso',
+    color: Colors.grey,
+  ),
+  Category(
+    id: 'c9',
+    title: 'great',
+    color: Colors.orange,
+  ),
+  Category(
+    id: 'c10',
+    title: 'chinese',
+    color: Colors.brown,
+  ),
+];
